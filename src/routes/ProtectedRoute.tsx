3e20d39e7f18f45a24e423a8abfbf8f2bd954960
“nathan.barbosa” <nathan.barbosa@teamsoft.com.br>
2025-03-26 00:29:05 -0300

    feature: Protected Route validation to login

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,9 +1,38 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 const ProtectedRoute = () => {
-  const isAuthenticated = !!localStorage.getItem("token");
+  const [loading, setLoading] = useState(true);
+  const [authenticated, setAuthenticated] = useState(false);
 
-  return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const res = await fetch('http://localhost:5068/v1/identity/me', {
+          credentials: 'include',
+        });
+
+        const json = await res.json();
+
+        if (res.ok && json.code === 200) {
+          setAuthenticated(true);
+        } else {
+          setAuthenticated(false);
+        }
+      } catch (err) {
+        console.error('Erro ao verificar autenticação:', err);
+        setAuthenticated(false);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    checkAuth();
+  }, []);
+
+  if (loading) return <div>Carregando...</div>;
+
+  return authenticated ? <Outlet /> : <Navigate to="/login" replace />;
 };
 
 export { ProtectedRoute };
