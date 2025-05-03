4f9726899df95ab8dca953cebb1d82a939ec1a3b
Pedro Henrique Souza <pdro@demant.com>
2025-05-03 13:41:18 -0300

    Fix: update api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_BASEURL || "https://191.252.60.126:8443",
+  baseURL: import.meta.env.VITE_BASEURL || "https://api.phsouza.com.br",
 });
