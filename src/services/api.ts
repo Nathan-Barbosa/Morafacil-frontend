2df27f1a8b9f1ba60d4ca71c99e228b3290a507c
Pedro Henrique Souza <52401796+ph-souzaa@users.noreply.github.com>
2025-05-03 23:17:03 -0300

    Fix: settings api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_BASEURL || "https://api.phsouza.com.br",
+  baseURL: import.meta.env.VITE_BASEURL || "http://localhost:5068/",
 });
